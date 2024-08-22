c8d53d23ea1488d56e643c98525d66115206867c
Giri2111 <vpecgirinath@gmail.com>
2024-08-22 12:23:20 +0530

    update 1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export default function Header()
         <a href="#" className="font-bold text-white">JIFFYTECH</a>
         <nav className="hidden md:block">
         <ul className="flex text-white">
-            <li className=' hover:text-black'><a href="/">Home</a></li>
+            <li className=' hover:text-black'><a href="#">Home</a></li>
             <li className=' hover:text-black'><a href="#about">About</a></li>
             <li className=' hover:text-black'><a href="#projects">Projects</a></li>
             <li className=' hover:text-black'><a href="#contact">Contact</a></li>
@@ -14,7 +14,7 @@ export default function Header()
         </nav>
          {navtool && <nav className="block md:hidden ">
         <ul onClick={()=>Setnavtool(!navtool)} className="flex flex-col text-white mobile-nav">
-            <li><a href="/">Home</a></li>
+            <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
